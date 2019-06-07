9ac86927533deef2a017eff47baccba05f1cb214
Alberto De La Paz <danieldlpxxx@gmail.com>
2019-06-07 11:44:30 -0500

    Nueva estructura de carpetas y archivos para GraphQL

--- index.js
+++ index.js
@@ -3,23 +3,20 @@
 const { graphql, buildSchema } = require('graphql')
 const express = require ('express')
 const gplMiddleware = require('express-graphql')
+const { readFileSync } = require ('fs')
+const { join } = require ('path')
+const resolvers = require ('./lib/resolvers.js')
 
 const app = express()
 const port = process.env.port || 4000
 
 //esquema inicial
-const schema = buildSchema(`
-    type Query {
-        hello: String
-    }
-`)
-
-//configurar resolvers
-const resolvers = {
-    hello: () => {
-        return 'Hola mundo'
-    }
-}
+const schema = buildSchema(
+    readFileSync(
+        join(__dirname,'lib','schema.graphql'),
+        'utf-8'
+    )
+)
 
 app.use ('/api',gplMiddleware({
     schema : schema,
@@ -32,6 +29,7 @@ app.listen (port, () => {
 })
 
 //ejecutar
-graphql( schema, '{hello}', resolvers).then ((data)=>{
-    console.log(data)
+
+graphql( schema, '{courses}', resolvers).then ((data)=>{
+    //console.log(data)
 })
