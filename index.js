8bd635b0a657950f3bfabe885a32f71029676004
Alberto De La Paz <danieldlpxxx@gmail.com>
2019-06-25 18:47:21 -0500

    agregando validacion de productivo

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 require ('dotenv').config()
 const { makeExecutableSchema } = require('graphql-tools')
 const express = require ('express')
+const cors = require ('cors')
 const gplMiddleware = require('express-graphql')
 const { readFileSync } = require ('fs')
 const { join } = require ('path')
@@ -10,6 +11,8 @@ const resolvers = require ('./lib/resolvers.js')
 
 const app = express()
 const port = process.env.port || 4000
+//para usar lo siguiente en windows npm i -g win-node-env
+const isDev = process.env.NODE_ENV !== 'production'
 
 //definiendo el esquema para graphql-tools
 const typeDefs = readFileSync(
@@ -19,10 +22,12 @@ const typeDefs = readFileSync(
 
 const schema = makeExecutableSchema({typeDefs, resolvers})
 
+app.use(cors())
+
 app.use ('/api',gplMiddleware({
     schema : schema,
     rootValue : resolvers,
-    graphiql : true
+    graphiql : isDev
 }))
 
 app.listen (port, () => {
